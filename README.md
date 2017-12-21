fcade30425ae99e0f696b6aac3c33adc7a523bc2
EmmaYXY <guyuegwqian@163.com>
2017-12-21 20:21:47 +0800

    step4:make migrations and migrate

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # blogproject
 a novice practice of django to develop a blog.
+
+- 在迁移数据库表至数据库中，得到如下错误：
+
+`TypeError: __init__() missing 1 required positional argument: 'on_delete'`
+
+后去stackoverflow网站找到答案，需要
+
+```
+categorie = models.ForeignKey(
+    'Categorie',
+    on_delete=models.CASCADE,
+)
+```
+
+但执行`python manage.py makemigrations`时依旧错误，后才发现ForeignKey有两个，我只改了一个。这是一个想当然的错误。
--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 
+	def __str__(self):
+		return self.name
+
 
 class Tag(models.Model):
 	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
 		
 
 class Post(models.Model):
@@ -22,10 +28,14 @@ class Post(models.Model):
 
 	#文章的数据库表和分类、标签的数据库表关联起来，但要根据需求采取不同
 	#的关联方式
-	category = models.ForeignKey(Category)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	tags = models.ManyToManyField(Tag, blank=True)
 
 	#关联Django内置应用django.contrib.auth中的用户模型User
-	author = models.ForeignKey(User)
+	#on_delete=models.DO_NOTHING是另一个选择，但不要忘记这个参数
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
 
 # Create your models here.
