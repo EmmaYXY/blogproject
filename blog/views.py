028d54a7c3731413c51c93da952d659a8256ddbc
EmmaYXY <guyuegwqian@163.com>
2017-12-23 16:15:26 +0800

    step11:category and archive

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
+from blog.models import Post, Category
 import markdown
 
 
@@ -15,6 +15,17 @@ def detail(request, pk):
 								  			  'markdown.extensions.toc',
 								  ])
 	return render(request, 'blog/detail.html', context={'post':post})
+
+def archives(request, year, month):
+	post_list = Post.objects.filter(created_time__year=year,
+									created_time__month=month).order_by('-created_time')
+	return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+	cate = get_object_or_404(Category, pk=pk)
+	post_list = Post.objects.filter(category=cate).order_by('-created_time')
+	return render(request, 'blog/index.html', context={'post_list': post_list})
+
 		                 
 		         
 
