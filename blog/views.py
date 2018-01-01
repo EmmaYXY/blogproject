d21a1ab47a128ff29da4dad65aa02655c789c69b
EmmaYXY <guyuegwqian@163.com>
2018-01-02 07:52:08 +0800

    step25:simple search

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ import markdown
 from django.views.generic import ListView, DetailView
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
+from django.db.models import Q
 
 
 class IndexView(ListView):
@@ -199,6 +200,7 @@ class CategoryView(ListView):
 		return super(CategoryView, self).get_queryset().filter(category=cate)
 
 
+
 class TagView(ListView):
 	model = Post
 	template_name = 'blog/index.html'
@@ -206,8 +208,21 @@ class TagView(ListView):
 
 	def get_queryset(self):
 		tag =  get_object_or_404(Tag, pk=self.kwargs.get('pk'))
-		return super(TagView, self).get_queryset().filter(tags=tag)		                 
-		         
+		return super(TagView, self).get_queryset().filter(tags=tag)		     
+
+
+
+def search(request):
+	q = request.GET.get('q')
+	error_msg = ''
+
+	if not q:
+		error_msg = '请输入关键词'
+		return render(request, 'blog/index.html', {'error_msg':error_msg})
+
+	post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))	
+	return render(request, 'blog/index.html', {'error_msg': error_msg,
+											   'post_list': post_list})	         
 
 
 # Create your views here.
