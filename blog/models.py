0dcb2c2ca1dc1cc86050c976c864f6ea48f545c8
EmmaYXY <guyuegwqian@163.com>
2018-01-01 10:26:26 +0800

    step17:generate excerpt automatically

--- blog/models.py
+++ blog/models.py
@@ -22,19 +22,19 @@ class Post(models.Model):
 
 	body = models.TextField()
 
-	#文章创建时间和最后一次修改时间
+	# 文章创建时间和最后一次修改时间
 	created_time = models.DateTimeField()
 	modified_time = models.DateTimeField()
 
 	excerpt = models.CharField(max_length=200, blank=True)
 
-	#文章的数据库表和分类、标签的数据库表关联起来，但要根据需求采取不同
-	#的关联方式
+	# 文章的数据库表和分类、标签的数据库表关联起来，但要根据需求采取不同
+	# 的关联方式
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	tags = models.ManyToManyField(Tag, blank=True)
 
-	#关联Django内置应用django.contrib.auth中的用户模型User
-	#on_delete=models.DO_NOTHING是另一个选择，但不要忘记这个参数
+	# 关联 Django 内置应用 django.contrib.auth 中的用户模型 User
+	# on_delete=models.DO_NOTHING 是另一个选择，但不要忘记这个参数
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	views = models.PositiveIntegerField(default=0)
@@ -49,6 +49,21 @@ class Post(models.Model):
 		self.views += 1
 		self.save(update_fields=['views'])
 
+	def save(self, *args, **kwargs):
+		# 如果没有填写摘要
+		if not self.excerpt:
+			# 先实例化一个 Markdown 的类，用于渲染 body 的文本
+			md = markdown.Markdown(extensions=[
+				'markdown.extensions.extra',
+				'markdown.extensions.codehilite',
+				])
+				# 先将 Markdown 文本渲染成 HTML 文本
+				# strip_tags 去掉 HTML 文本的全部HTML标签
+				# 从文本摘取前 54 个字符赋给 excerpt
+			self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+			# 调用父类 save 方法将数据保存到数据库中
+		super(Post, self).save(*args, **kwargs)
 	class Meta:
 		ordering = ['-created_time']
 
