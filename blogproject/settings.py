7f52094de16e19e63cb6cc38a6b2676ff32f140d
EmmaYXY <guyuegwqian@163.com>
2018-01-04 19:28:36 +0800

    DEBUG=False

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1@!3rv1ex*_^ts9z*yrj^#3-6rsw(!%@i_0_&!9xkm(!wz%9k2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.emmayxy.store']
 
